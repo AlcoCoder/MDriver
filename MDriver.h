aead227d326f60ae662d9574136a1826e56aba8a
AlcoCoder <dubinin.lexa@yandex.ru>
2017-08-27 17:12:02 +0300

    Add files via upload

---
+++ MDriver.h
@@ -0,0 +1,22 @@
+#include "Arduino.h"
+
+class MDriver
+{
+  public:
+    MDriver(uint8_t EN, uint8_t IN1, uint8_t IN2);
+    MDriver(uint8_t IN1, uint8_t IN2);
+    
+    void speed(byte speed);
+    void forward();
+    void backward();
+    void stop();
+    void fast_stop();
+    
+  private:
+    bool _pwm;
+    uint8_t _en;
+    uint8_t _in1;
+    uint8_t _in2;
+    uint8_t _speed;
+};
+
